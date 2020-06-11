113723aaf0e5751da3abb458bedc28e7daf2fe84
umeharaumeo <66778980+umeharaumeo@users.noreply.github.com>
2020-06-11 21:33:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,13 +5,16 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+    
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視
+    if message.author.bot:
+        return
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
+    if message.content == 'こい':
+           await message.channel.send('ボイスチャンネルに参加しろ')
+            
 
 @bot.command()
 async def ping(ctx):
