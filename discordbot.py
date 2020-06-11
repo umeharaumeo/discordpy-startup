a81d6849da9c2d54cfa957fb414b23fa29813594
umeharaumeo <66778980+umeharaumeo@users.noreply.github.com>
2020-06-11 19:52:02 +0900

    implement おはよう

--- discordbot.py
+++ discordbot.py
@@ -17,7 +17,7 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
-    @bot.command()
+@bot.command()
 async def おはよう(ctx):
     await ctx.send('にっこにっこに～！')
 
