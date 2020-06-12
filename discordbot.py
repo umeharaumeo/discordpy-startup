e1cbfc87948b9efa5092c95b54ea35fadc135e8c
umeharaumeo <66778980+umeharaumeo@users.noreply.github.com>
2020-06-12 12:35:19 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -37,9 +37,6 @@ async def on_message(message):
 
     if message.content == 'すき':
         await message.channel.send('らぶにこっ(⋈◍＞◡＜◍)。✧♡')
-        
-        
-
-       
+   
         
 bot.run(token)
