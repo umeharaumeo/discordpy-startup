8398022ff8dc00a0169b0f93eaf03c9645eaca6e
umeharaumeo <66778980+umeharaumeo@users.noreply.github.com>
2020-06-13 02:06:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -64,6 +64,10 @@ async def on_message(message):
         await message.channel.send('だまれ')
         print(message.author.345226218309681173)
         
+    if message.content == 'すき':
+        await message.channel.send('わたしも♡')
+        print(message.author.415854895560917002)
+        
 
 
 # Botの起動とDiscordサーバーへの接続
